4b935945ed5cd7bd629ebbe5b1032aec4e49ebe1
J M Rossy <jm.rossy@gmail.com>
2022-10-22 19:17:52 -0400

    Tweak debug message strings

--- src/features/debugger/strings.ts
+++ src/features/debugger/strings.ts
@@ -4,9 +4,9 @@ export const debugStatusToDesc: Record<MessageDebugStatus, string> = {
   [MessageDebugStatus.AlreadyProcessed]: 'No errors found, message already processed',
   [MessageDebugStatus.NoErrorsFound]: 'No errors found, message appears to be deliverable.',
   [MessageDebugStatus.InvalidDestDomain]: 'The destination domain id is invalid.',
-  [MessageDebugStatus.UnknownDestChain]: `Destination chain is not included in this message's environment.`,
+  [MessageDebugStatus.UnknownDestChain]: `Destination chain is not in this message's environment.`,
   [MessageDebugStatus.RecipientNotContract]: 'Recipient address is not a contract.',
   [MessageDebugStatus.RecipientNotHandler]:
-    'Recipient bytecode does not contain handle function selector',
+    'Recipient bytecode is missing handle function selector',
   [MessageDebugStatus.HandleCallFailure]: 'Error calling handle on the recipient contract',
 };
