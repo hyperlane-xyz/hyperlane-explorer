6d520ef7cf19c427d1a49b01519668953406ec0c
J M Rossy <jm.rossy@gmail.com>
2023-01-12 12:31:36 -0500

    Update banner text

--- src/components/nav/InfoBanner.tsx
+++ src/components/nav/InfoBanner.tsx
@@ -6,7 +6,7 @@ export function InfoBanner() {
       rel="noopener noreferrer"
       className="block py-1.5 w-full text-white text-center text-sm bg-blue-600 hover:bg-blue-700 active:bg-blue-800 ring-1 ring-inset ring-green-700 transition-all duration-300"
     >
-      This is the Explorer for Hyperlane version 1.{' '}
+      This is the Explorer for Hyperlane version 1 (Deprecated).{' '}
       <span className="underline underline-offset-2">Use version 2</span>
     </a>
   );
