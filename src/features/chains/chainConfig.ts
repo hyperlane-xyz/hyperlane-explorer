862c54e929127d30546021bc78dbcd21e21d15d1
J M Rossy <jm.rossy@gmail.com>
2023-03-09 12:24:30 +0100

    Update SDK and widgets version Offload metadata validation to SDK

--- src/features/chains/chainConfig.ts
+++ src/features/chains/chainConfig.ts
@@ -1,58 +1,12 @@
 import { z } from 'zod';
 
-import { ChainMetadata, chainIdToMetadata as defaultChainIdToMetadata } from '@hyperlane-xyz/sdk';
+import {
+  ChainMetadata,
+  ChainMetadataSchema,
+  chainIdToMetadata as defaultChainIdToMetadata,
+} from '@hyperlane-xyz/sdk';
 
 import { logger } from '../../utils/logger';
-
-// TODO move to SDK?
-// Should match ChainMetadata from SDK
-export const chainMetadataSchema = z.object({
-  chainId: z.number(),
-  domainId: z.number().optional(),
-  name: z.string(),
-  displayName: z.string().optional(),
-  displayNameShort: z.string().optional(),
-  nativeToken: z
-    .object({
-      name: z.string(),
-      symbol: z.string(),
-      decimals: z.number(),
-    })
-    .optional(),
-  publicRpcUrls: z
-    .array(
-      z.object({
-        http: z.string(),
-        webSocket: z.string().optional(),
-        pagination: z
-          .object({
-            blocks: z.number(),
-            from: z.number(),
-          })
-          .optional(),
-      }),
-    )
-    .nonempty(),
-  blockExplorers: z.array(
-    z.object({
-      name: z.string(),
-      url: z.string(),
-      apiUrl: z.string().optional(),
-      family: z.string().optional(),
-    }),
-  ),
-  blocks: z
-    .object({
-      confirmations: z.number(),
-      reorgPeriod: z.number().optional(),
-      estimateBlockTime: z.number().optional(),
-    })
-    .optional(),
-  transactionOverrides: z.object({}).optional(),
-  gasCurrencyCoinGeckoId: z.string().optional(),
-  gnosisSafeTransactionServiceUrl: z.string().optional(),
-  isTestnet: z.boolean().optional(),
-});
 
 export const chainContractsSchema = z.object({
   mailbox: z.string(),
@@ -61,8 +15,10 @@ export const chainContractsSchema = z.object({
   // interchainAccountRouter: z.string().optional(),
 });
 
-export const chainConfigSchema = chainMetadataSchema.extend({ contracts: chainContractsSchema });
 export type ChainConfig = ChainMetadata & { contracts: z.infer<typeof chainContractsSchema> };
+export const chainConfigSchema = ChainMetadataSchema.extend({
+  contracts: chainContractsSchema,
+});
 
 type ParseResult =
   | {
--- src/features/messages/cards/TimelineCard.tsx
+++ src/features/messages/cards/TimelineCard.tsx
@@ -18,7 +18,6 @@ export function TimelineCard({ message, resolvedStatus, resolvedDestinationTx }:
     [message, resolvedStatus, resolvedDestinationTx],
   );
 
-  // @ts-ignore allow Unknown status in message state
   const { stage, timings } = useMessageStage({ message });
 
   return (
@@ -28,7 +27,6 @@ export function TimelineCard({ message, resolvedStatus, resolvedDestinationTx }:
         <HelpIcon size={16} text="A breakdown of the stages for delivering a message" />
       </div> */}
       <div className="sm:px-2">
-        {/* @ts-ignore allow Unknown status in message state */}
         <MessageTimeline status={resolvedMessage.status} stage={stage} timings={timings} />
       </div>
     </Card>
