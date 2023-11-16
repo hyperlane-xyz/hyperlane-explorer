bd265b4e66729a572d7b6238d710aca3e71a0215
J M Rossy <jm.rossy@gmail.com>
2023-11-16 17:42:42 -0500

    Add info banner

--- src/components/nav/InfoBanner.tsx
+++ src/components/nav/InfoBanner.tsx
@@ -1,13 +1,13 @@
 export function InfoBanner() {
   return (
     <a
-      href="https://explorer-v1.hyperlane.xyz"
+      href="https://explorer-v2.hyperlane.xyz"
       target="_blank"
       rel="noopener noreferrer"
       className="block py-1.5 w-full text-white text-center text-sm bg-blue-600 hover:bg-blue-700 active:bg-blue-800 ring-1 ring-inset ring-green-700 transition-all duration-300"
     >
-      This is the Explorer for Hyperlane version 2.{' '}
-      <span className="underline underline-offset-2">Use version 1</span>
+      This is the explorer for Hyperlane version 3.{' '}
+      <span className="underline underline-offset-2">Use version 2</span>
     </a>
   );
 }
