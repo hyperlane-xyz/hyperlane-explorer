2ba5c89f687f9e147e5f9d22875e99c2b4d77613
J M Rossy <jm.rossy@gmail.com>
2024-02-12 17:39:50 -0500

    Updgrade to 3.7.0 libs

--- src/components/icons/ChainLogo.tsx
+++ src/components/icons/ChainLogo.tsx
@@ -5,13 +5,9 @@ import { ChainLogo as ChainLogoInner } from '@hyperlane-xyz/widgets';
 import { getChainName } from '../../features/chains/utils';
 import { useMultiProvider } from '../../features/providers/multiProvider';
 
-// TODO widget lib for new chainid type
-type Props = Omit<ComponentProps<typeof ChainLogoInner>, 'chainId'> & { chainId: number | string };
-
-export function ChainLogo(props: Props) {
+export function ChainLogo(props: ComponentProps<typeof ChainLogoInner>) {
   const { chainName, chainId, ...rest } = props;
   const multiProvider = useMultiProvider();
   const name = chainName || getChainName(multiProvider, props.chainId);
-  const chainIdNumber = typeof chainId === 'number' ? chainId : undefined;
-  return <ChainLogoInner {...rest} chainName={name} chainId={chainIdNumber} />;
+  return <ChainLogoInner {...rest} chainName={name} chainId={chainId} />;
 }
