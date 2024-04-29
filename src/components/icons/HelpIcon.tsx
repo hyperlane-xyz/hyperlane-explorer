2ed49779b7faea383f9e23647dac0bbb71630ffc
J M Rossy <jm.rossy@gmail.com>
2024-04-29 10:14:42 -0400

    Use tooltip instead of toast for show help information  (#70)
    Co-authored-by: tiendn <tiendn@stellalab.com>

--- src/components/icons/HelpIcon.tsx
+++ src/components/icons/HelpIcon.tsx
@@ -1,22 +1,21 @@
 import { memo } from 'react';
-import { toast } from 'react-toastify';
 
 import Question from '../../images/icons/question-circle.svg';
 import { IconButton } from '../buttons/IconButton';
 
 function _HelpIcon({ text, size = 20 }: { text: string; size?: number }) {
-  const onClick = () => {
-    toast.info(text, { autoClose: 8000 });
-  };
-
   return (
     <IconButton
       imgSrc={Question}
       title="Help"
       width={size}
       height={size}
-      onClick={onClick}
       classes="opacity-50"
+      passThruProps={{
+        'data-tooltip-content': text,
+        'data-tooltip-id': 'root-tooltip',
+        'data-tooltip-place': 'top-start',
+      }}
     />
   );
 }
