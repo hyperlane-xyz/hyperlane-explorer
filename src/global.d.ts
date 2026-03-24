f1254d2b9a0673f7179acdbfaf2851715dd6c8a6
Paul Balaji <10051819+paulbalaji@users.noreply.github.com>
2026-03-24 13:39:27 +0000

    feat: upgrade to TypeScript 6 (#299)

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,3 +4,8 @@ declare type ChainId = number | string;
 declare type DomainId = number;
 declare type AddressTo<T> = Record<Address, T>;
 declare type Fn = () => void;
+
+declare module '*.css' {
+  const content: string;
+  export default content;
+}
