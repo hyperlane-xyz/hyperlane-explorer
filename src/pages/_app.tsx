454935ade5b34a9a489dfeb1252cd18bb8b3bd90
pbio <10051819+paulbalaji@users.noreply.github.com>
2026-08-10 11:34:56 +0100

    fix: restore Tailwind 3 compatibility

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ import {
   fetchExchange,
 } from 'urql';
 
+import '@hyperlane-xyz/widgets/styles.css';
 import { AppLayout } from '../AppLayout';
 import { AppErrorBoundary } from '../components/errors/AppErrorBoundary';
 import { AppLoadingShell } from '../components/layout/AppLoadingShell';
