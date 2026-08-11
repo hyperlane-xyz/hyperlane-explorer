f03e07165c45d1435522af651124fe9e603acc18
Troy Kessler <troy.kessler99@gmail.com>
2026-08-11 18:35:22 +0200

    fix: add env for api url

--- src/consts/config.ts
+++ src/consts/config.ts
@@ -1,5 +1,7 @@
 const isDevMode = process.env.NODE_ENV === 'development';
 const version = process.env.NEXT_PUBLIC_VERSION ?? null;
+const apiUrl =
+  process.env.NEXT_PUBLIC_API_URL || 'https://explorer4.hasura.app/v1/graphql';
 const registryUrl = process.env.NEXT_PUBLIC_REGISTRY_URL || undefined;
 const registryBranch = process.env.NEXT_PUBLIC_REGISTRY_BRANCH || 'main';
 const explorerApiKeys = JSON.parse(process.env.EXPLORER_API_KEYS || '{}');
@@ -17,7 +19,7 @@ interface Config {
 export const config: Config = Object.freeze({
   debug: isDevMode,
   version,
-  apiUrl: 'https://explorer4.hasura.app/v1/graphql',
+  apiUrl,
   explorerApiKeys,
   githubProxy: 'https://proxy.hyperlane.xyz',
   registryBranch,
